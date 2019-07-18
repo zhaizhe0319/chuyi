44a5a0b81e13e05939b86fdce8e5375efbfef6ff
chuyi <444169475@qq.com>
2019-07-18 11:07:04 +0800

    init add

---
+++ httpserver.py
@@ -0,0 +1,40 @@
+"""
+httpserver v1.0
+基本要求：1.获取来自浏览器的请求
+　　　　　2.判断如果请求内容是/ 将index.html返回给客户端
+　　　　　3.如果请求的是其他内容则返回404　
+"""
+
+from socket import *
+
+#　客户端(浏览器)处理
+def request(connfd):
+    # 获取请求将请求内容提取出来
+    data=connfd.recv(4096)
+    request_line=data.decode().split("\n")[0]
+
+    # 判断是/ 则返回index.html 不是则返回404
+    if info == '/':
+        with open("index.html") as f:
+            response="HTTP/1.1 200 OK\r\n"
+            response+="Content-Type:text/html\r\n"
+            response+="\r\n"
+            response+=f.read()
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n"
+        response += "Content-Type:text/html\r\n"
+        response += "\r\n"
+        response += "<h1>Sorry....</h1>"
+    connfd.send(response.encode())
+   
+
+
+#　搭建ｔｃｐ网络
+sockfd = socket()
+sockfd.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+sockfd.bind(('0.0.0.0',8000))
+sockfd.listen(3)
+while True:
+    connfd,addr = sockfd.accept()
+    request(connfd) #　处理客户端请求
+
---
+++ listtest.py
@@ -0,0 +1,8 @@
+from jiedian import *
+
+l = []
+for i in range(99999):
+    l.append(i)
+
+link = Linklist
+link.init_list(l)
